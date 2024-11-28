577d7dc534ee9b1012157b0ccfeb39aece7cfa6b
latestbob <edidiongbobson@gmail.com>
2024-11-28 20:00:57 +0100

    feat(update nonclinical staff get requests)

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import isAuthenticated from '../middlewares/authenticated';
-import { changePassword, updateProfile } from '../controllers/userController';
+import { changePassword, getNonClinicalStaff, getUniqueUser, updateProfile } from '../controllers/userController';
 import { validateChangePassword } from '../middlewares/userMiddleware';
 
 const userRouter = Router();
@@ -14,6 +14,18 @@ userRouter.put('/profile', isAuthenticated, updateProfile);
 
 //change password
 userRouter.put('/changepassword', isAuthenticated, changePassword);
+
+
+//get all users that are not doctors or nurse
+
+//
+
+userRouter.get("/nonclincalstaff", getNonClinicalStaff)
+
+//get unique user staff by uuid
+
+userRouter.get("/unique/:uuid", isAuthenticated, getUniqueUser);
+
 
 
 export default userRouter;
